6f9e3d2271b7f7f54a5cb4d9a1517ba3bd28f41b
Ivan Dedov <evan4mc@gmail.com>
2025-03-17 13:15:08 +0500

    add hero section

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,13 @@
+<section class="hero relative bg-cover bg-center bg-no-repeat h-80 mx-auto flex items-center">
+   <div class="absolute left-0 top-0 w-full h-full z-1 bg-black/80 overlay"></div>
+   <div class="container mx-auto text-center z-10">
+    <h2 class="text-4xl md:text-5xl text-white font-bold mb-8">Find Your Dream Job</h2>
+    <form class="block mx-5 md:mx-auto md:space-x-2">
+      <input type="text" name="keywords" placeholder="Keywords" class="w-full md:w-72 px-4 py-3 focus:outline-none bg-white">
+      <input type="text" name="location" placeholder="Location" class="w-full md:w-72 px-4 py-3 focus:outline-none bg-white">
+      <button class="md:auto bg-blue-700 hover:bg-blue-600 text-white px-4 py-3 focus:outline-none">
+        <i class="fa-solid fa-search mr-1"></i> Search
+      </button>
+    </form>
+   </div>
+</section>
